ea7619e33ed62f30ab291a785bb36182429d88a2
Brandon Roberts <robertsbt@gmail.com>
2024-03-13 11:55:14 -0500

    fix: Added Vitest setup fixes

--- packages/admin-ui/vite.config.ts
+++ packages/admin-ui/vite.config.ts
@@ -1,18 +1,24 @@
 /// <reference types="vitest" />
 
 import angular from '@analogjs/vite-plugin-angular';
+import viteTsconfigPaths from 'vite-tsconfig-paths';
 import { defineConfig } from 'vite';
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => {
     return {
         root: __dirname,
-        plugins: [angular()],
+        plugins: [
+            angular(),
+            viteTsconfigPaths({
+                root: '.',
+            }),
+        ],
         test: {
             globals: true,
             environment: 'jsdom',
             setupFiles: ['src/test-setup.ts'],
-            include: ['**/temp.spec.ts'],
+            include: ['**/*.spec.ts'],
             reporters: ['default'],
             passWithNoTests: false,
         },
